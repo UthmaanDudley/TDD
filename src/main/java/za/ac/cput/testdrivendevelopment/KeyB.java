8e1e54a341c2817a440420e611343ff6ea0ede56
M.Nur <nurduncan12345@gmail.com>
2021-04-01 21:34:06 +0200

    My Third Commit

--- src/main/java/za/ac/cput/testdrivendevelopment/KeyB.java
+++ src/main/java/za/ac/cput/testdrivendevelopment/KeyB.java
@@ -7,8 +7,38 @@ package za.ac.cput.testdrivendevelopment;
 
 /**
  *
- * @author M. Nur
+ * @author Moegamad Nur Duncan 220014442
  */
 public class KeyB {
+    public String Serialn, color, Keyn;
+
+    public String getSerialn() {
+        return Serialn;
+    }
+
+    public void setSerialn(String Serialn) {
+        this.Serialn = Serialn;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getKeyn() {
+        return Keyn;
+    }
+
+    public void setKeyn(String Keyn) {
+        this.Keyn = Keyn;
+    }
+
+    @Override
+    public String toString() {
+        return "KeyB{" + "Serialn=" + Serialn + ", color=" + color + ", Keyn=" + Keyn + '}';
+    }
     
 }
--- src/test/java/za/ac/cput/testdrivendevelopment/KeyBTest.java
+++ src/test/java/za/ac/cput/testdrivendevelopment/KeyBTest.java
@@ -4,43 +4,66 @@
  * and open the template in the editor.
  */
 package za.ac.cput.testdrivendevelopment;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 /**
  *
- * @author M. Nur
+ * @author 220014442 Moegamad Nur Duncan
  */
 public class KeyBTest {
     
-    public KeyBTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
+    public KeyB keyb1;
+    public KeyB keyb2;
+    public KeyB keyb3;
     
     @BeforeEach
     public void setUp() {
+        keyb1 = new KeyB();
+        keyb2 = new KeyB();
+        keyb3 = keyb2;        
     }
     
-    @AfterEach
-    public void tearDown() {
+    @Test
+    void testIden(){
+        assertSame(keyb2, keyb3);
+    }
+    //
+    @Test
+    void testEqual(){
+        assertEquals(keyb2, keyb3);
+    }
+     
+    @Test
+    void failTest(){
+        if (keyb3 ==keyb2){
+        fail(" Deliberately fail keyb3 is equal to keyb2");
+    }
+    }
+    
+    @Disabled("Disabled until keyb1 is sold")
+    @Test
+    void disableTest() {
+        assertSame(keyb1, keyb3);
+    
+    }
+    
+    
+    @Timeout (1)
+    @Test
+    public void testWithTimeout(){
+       for (int i=0;i<100000;i++)
+       {
+           System.out.println(i);
+       }
+    
+    
+    
+    
     }
 
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
+
